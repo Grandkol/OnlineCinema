f4e5da875e2e1438793fadbf7ec5f6c864091025
stupenko <stupenko@ivk.ru>
2024-09-05 17:12:35 +0300

    Импортировал модель персон

--- fastapi-service/src/main.py
+++ fastapi-service/src/main.py
@@ -4,6 +4,7 @@ from fastapi import FastAPI
 from fastapi.responses import ORJSONResponse
 from redis.asyncio import Redis
 
+from .api.v1 import persons
 from .api.v1 import films
 from .core import config
 from .db import elastic
