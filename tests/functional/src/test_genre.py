efc0adbbc6c7884bd5461b43c825d213a4877248
Stkos95 <stkos95@yandex.ru>
2024-09-19 20:54:29 +0300

    Сделал тест всех жанров

--- tests/functional/src/test_genre.py
+++ tests/functional/src/test_genre.py
@@ -16,61 +16,39 @@ from functional.conftest import (
     bulk_query,
     _load_schema,
 )
-from functional.testdata.person_data import SEARCH_PERSON_DATA, PERSON_DATA
+from functional.testdata.genre_data import GENRE_DATA
 
-API_PERSON_SEARCH = "persons/search"
+API_ALL_GENREs = "genres/"
 API_FILMS = "persons/"
 SCHEMA = _load_schema("persons")
 
 
 @pytest.mark.parametrize(
-    "query_data, expected_answer, response",
+    "query_data, expected_answer, response_template",
     [
         (
-                {"page_number": "2", "page_size": "1"},
-                {"status": 200, "length": 1},
-                  {
-                    "id": "237fd1e4-c98e-454e-aa13-8a13fb7547b5",
-                    "name": "Romance"
-                    }
+            {"page_number": "2", "page_size": "1"},
+            {"status": 200, "length": 1},
+            {"id": "237fd1e4-c98e-454e-aa13-8a13fb7547b5", "name": "Romance"},
         ),
-                {"page_number": "1", "page_size": "50"},
-                {"status": 200, "length": 1},
-                None
-        # (
-        #         {"query": "Nick"},
-        #         {"status": 200, "length": 22}
-        # ),
-        # (
-        #         {"query": "Nick", 'page_number': 100, 'page_size': 2},
-        #         {"status": 404, "length": 1}
-        # ),
-        # (
-        #         {"query": "John", 'page_number': 1, 'page_size': 4},
-        #         {"status": 200, "length": 4}
-        # ),
-        # (
-        #     {"query": "John", "page_number": 0, "page_size": 0},
-        #     {"status": 422, "length": 1},
-        # ),
-        # (
-        #         {"query": "Nick", 'page_number': 'ererer', 'page_size': 2},
-        #         {"status": 422, "length": 1}
-        # ),
-        # (
-        #         {"query": "Nick", 'page_number': 1, 'page_size': 'qw'},
-        #         {"status": 422, "length": 1}
-        # ),
+        ({"page_number": "1", "page_size": "50"}, {"status": 200, "length": 26}, None),
     ],
 )
 @pytest.mark.asyncio
-async def test_person_search(
-    es_write_data, make_get_request, bulk_query, query_data, expected_answer
+async def test_genre_search(
+    es_write_data,
+    make_get_request,
+    bulk_query,
+    query_data,
+    expected_answer,
+    response_template,
 ):
-    
-    bulk_query = bulk_query("persons", PERSON_DATA)
+
+    bulk_query = bulk_query("genres", GENRE_DATA)
     await es_write_data(SCHEMA, "persons", bulk_query)
     asyncio.sleep(1)
-    response = await make_get_request(API_PERSON_SEARCH, query_data)
+    response = await make_get_request("genres", query_data)
     assert response[1] == expected_answer["status"]
     assert len(response[0]) == expected_answer["length"]
+    if response_template:
+        assert response[0][0] == response_template
