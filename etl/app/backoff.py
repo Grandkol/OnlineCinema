847e07be5902cea11339850fa9e42cee82bbd30a
Stkos95 <stkos95@yandex.ru>
2024-09-20 23:08:20 +0300

    Сделал backoff

--- etl/app/backoff.py
+++ etl/app/backoff.py
@@ -8,7 +8,7 @@ from psycopg import OperationalError
 logger = MainLogger().get_logger("backoff")
 
 
-def backoff(start_sleep_time=0.1, factor=2, border_sleep_time=10):
+def backoff(start_sleep_time=0.1, factor=2, border_sleep_time=10, retries=10):
     """Функция для повторного подключения к базе данных.
         Постоянно увеличивает время ожидания.
 
@@ -20,23 +20,25 @@ def backoff(start_sleep_time=0.1, factor=2, border_sleep_time=10):
 
     def func_wrapper(func):
         @wraps(func)
-        def inner(instance, *args, **kwargs):
+        def inner(*args, **kwargs):
             time_to_sleep = 0
             counter = 0
             while True:
-                try:
-                    return func(instance, *args, **kwargs)
-                except (ConnectionError, OperationalError):
-                    time_to_sleep = start_sleep_time * (factor**counter)
-                    if time_to_sleep > border_sleep_time:
-                        time_to_sleep = border_sleep_time
+                res_func = func(*args, **kwargs)
+                if res_func:
+                    return res_func
+                time_to_sleep = start_sleep_time * (factor**counter)
+                time_to_sleep = min(time_to_sleep, border_sleep_time)
+                counter += 1
+                logger.info(
+                    f"Функция {func.__qualname__} завершилась с ошибкой. Повторяю попытку, жду {time_to_sleep}"
+                )
+                if counter > retries:
                     logger.info(
-                        f"Пробую подключиться к базе данных повторно повторно. Жду %s", time_to_sleep
+                        f"Функция {func.__qualname__} превысила количество попыток ({retries}). Завершено!"
                     )
-                    counter += 1
-                    time.sleep(time_to_sleep)
-                except KeyboardInterrupt:
-                    exit()
+                    break
+                time.sleep(time_to_sleep)
 
         return inner
 
